7bd45f34b3df9bdb0a3b4deb8f7d84c225580434
unknown <sivertsson.anton@gmail.com>
2015-05-13 22:53:21 +0200

    Anton - Pulat lite med förberedelser inför chat-funktionalitet

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//app.js
+$(function () {
+	//instantierar modellen i appen
+	var model = new model();
+	//Här instansierar vi resten av views och controllers
+});
--- js/model.js
+++ js/model.js
@@ -1,138 +1,173 @@
 //Model
 
+//var model = function () {
+  //Get UUID from user and specify PubNub keys
+  var usrLat;
+  var usrLng;
+  var observers = [];
+  var chatPartner = "hejsan"; //Reserverad för den person som man chattar med aktuellt
+  var chatRoom; //Det rum två chattande personer är i
 
-//Get UUID from user and specify PubNub keys
-var usrLat;
-var usrLng;
-
-var UUID = PUBNUB.db.get('session') || (function(){ 
-  var uuid = PUBNUB.uuid(); 
-  PUBNUB.db.set('session', uuid);
-  console.log(uuid);
-  return uuid; 
-})();
-
-
-var pubnub = PUBNUB.init({
-  	publish_key   : "pub-c-7fd548b4-2815-463d-9c1c-5187d45d49f5",
-  	subscribe_key : "sub-c-bea78536-e9a9-11e4-91d3-0619f8945a4f",
-  	uuid : UUID
-});
-var userId = UUID//pubnub.uuid();
-var users =[];
-var activeChannels = ['moo']
-
-setInterval(getLocation(), 1000);
-console.log(userId);
+  var UUID = PUBNUB.db.get('session') || (function(){ 
+    var uuid = PUBNUB.uuid(); 
+    PUBNUB.db.set('session', uuid);
+    console.log(uuid);
+    return uuid; 
+  })();
 
 
-function waitingRoom(){
-	// unsubscribe
-	//subscribe till det allmänna för gällande geo hash samt de intillliggande
-	//dvs subscrive till lat:long , lat+1:long, lat-1:long, lat:long+1 osv
-	//bevaka public channel
-	//for(var i in geoHash){}
-	unsubAll();
-	activeChannels.push("waitingRoom"); //byt med geo
-	pubnub.subscribe({
-    	'channel'   : 'waitingRoom', //byt ut sedan
-    	'callback'  : function(msg) {
-    		
-        	if (userId === msg.reciever || userId === msg.sender){
-        		console.log(msg);
-          		if(msg.mtype==="RES") {
-          			console.log('response detected');//notifyObservers(); //enterChat(msg.sender ,userId);
-          		}
-          		else if(msg.mtype==="REQ"){
-          			console.log('Request detected');//notifyObservers(); //respondToRequest(msg.sender);
-          		}
-          		else if(msg.mtype === "DEN"){
-          			console.log('Denial detected');//notifyObservers(); //sök efter ny partner?
-          		}
-        	}
-    	},
-    	presence: function(m){
-    		console.log(m);
-    		users.push(m.uuid);
+  var pubnub = PUBNUB.init({
+    	publish_key   : "pub-c-7fd548b4-2815-463d-9c1c-5187d45d49f5",
+    	subscribe_key : "sub-c-bea78536-e9a9-11e4-91d3-0619f8945a4f",
+    	uuid : UUID
+  });
+  var userId = UUID//pubnub.uuid();
+  var users =[];
+  var activeChannels = ['moo']
+
+  setInterval(getLocation(), 1000);
+  console.log(userId);
+
+
+  function waitingRoom(){
+  	// unsubscribe
+  	//subscribe till det allmänna för gällande geo hash samt de intillliggande
+  	//dvs subscrive till lat:long , lat+1:long, lat-1:long, lat:long+1 osv
+  	//bevaka public channel
+  	//for(var i in geoHash){}
+  	unsubAll();
+  	activeChannels.push("waitingRoom"); //byt med geo
+  	pubnub.subscribe({
+      	'channel'   : 'waitingRoom', //byt ut sedan
+      	'callback'  : function(msg) {
+      		
+          	if (userId === msg.reciever || userId === msg.sender){
+          		console.log(msg);
+            		if(msg.mtype==="RES") {
+            			console.log('response detected');//notifyObservers(); //enterChat(msg.sender ,userId);
+            		}
+            		else if(msg.mtype==="REQ"){
+            			console.log('Request detected');//notifyObservers(); //respondToRequest(msg.sender);
+            		}
+            		else if(msg.mtype === "DEN"){
+            			console.log('Denial detected');//notifyObservers(); //sök efter ny partner?
+            		}
+          	}
+      	},
+      	presence: function(m){
+      		console.log(m);
+      		users.push(m.uuid);
+      	}
+    	});
+  }
+
+  function unsubAll() {
+  	for(var x in activeChannels){
+  		pubnub.unsubscribe({'channel': activeChannels[x]})
+  		activeChannels = [];
+  	}
+  }
+
+  function requestChat(){
+  	//slumpa någon ur waitingRoom och skicka en request
+    if (users.length > 1) { //Om det finns andra att chatta med än en själv
+    	var chatPartner = randomElement(users); //Väljer en random chatpartner
+    	pubnub.publish({
+        'channel' : 'waitingRoom',
+        'message' : {"mtype": "REQ", "sender":userId, "reciever":chatPartner}
+    	});
+    }
+    else {
+      alert("There are no available partners in this area, please move.");
+    }
+  }
+
+  function acceptRequest(initiator){
+  	//slumpad användare blir tillfrågad
+  	//skicka bekräftelse tillbaka
+  	pubnub.publish({
+          'channel' : 'waitingRoom',
+          'message' : {"mtype": "RES", "sender":userId, "reciever":initiator}
+    	});
+    	enterChat(userId, initiator);
+  }
+
+  function denyRequest(initiator){
+  	//slumpad användare blir tillfrågad
+  	//skicka bekräftelse tillbaka
+  	pubnub.publish({
+          'channel' : 'waitingRoom',
+          'message' : {"mtype": "DEN", "sender":userId, "reciever":initiator}
+    	});
+  }
+
+  function randomElement() {
+  	//Randomizes users array and selects a random element
+  	var array = users;
+  	for (var id in array) {
+  		if (array[id] === userId){
+  			array.splice(id,1);
+  			break;
+  		}
+  	}
+  	var item = array[Math.floor(Math.random()*array.length)];
+    	return item; //selects a random element
+  }
+
+  function enterChat(uuidA,uuidB) {
+  	//Unsub från public channels
+  	//sub to unique (uuidA+UUidB)
+  	unsubAll();
+    pubnub.subscribe({
+      'channel'   : uuidA+''+uuidB,
+      'callback'  : function(msg) {
+        if (msg.sender === userId) {
+          $("#output").append(printMsg("sentMsg", msg.contents, msg.sender)); //senare ska vi skicka namn istället för msg.sender
+        }
+        else {
+          $("#output").append(printMsg("recievedMsg", msg.contents, msg.reciever));
+        }
+      }
+    });
+  }
+
+  function leaveChat() {
+    pubnub.unsubscribe({
+      'channel' : chatRoom //unsubscribear en från aktuellt chatrum
+    })
+  	//unsub från unique/s
+  	//call waitingRoom()
+
+  }
+
+  function getLocation() { //tar fram koordinater
+    	if (navigator.geolocation) {
+        	navigator.geolocation.getCurrentPosition(getCoord);
     	}
-  	});
-}
+    	else { 
+        	console.log("Geolocation is not supported by this browser.");
+    	}
+  }
 
-function unsubAll() {
-	for(var x in activeChannels){
-		pubnub.unsubscribe({'channel': activeChannels[x]})
-		activeChannels = [];
-	}
-}
+  function getCoord(position) { //Callback-funktion som hanterar
+  	usrLat = position.coords.latitude; //Fullösning att göra dessa publika?
+  	usrLng = position.coords.longitude;
+  	//geohashChannel = geohash(position.coords.latitude,2) + '' + geohash(position.coords.longitude,2); //Geohashar området till en kanal
+  	//console.log(geohashChannel);
+  }
 
-function requestChat(){
-	//slumpa någon ur waitingRoom och skicka en request
-	var randomReciever = randomElement(users);
-	pubnub.publish({
-        'channel' : 'waitingRoom',
-        'message' : {"mtype": "REQ", "sender":userId, "reciever":randomReciever}
-  	});
-}
+  function printMsg(id, msg, name){
+    return '<div id='+id+'>' + msg+" : "+name+'</div>';
+  }
 
-function acceptRequest(initiator){
-	//slumpad användare blir tillfrågad
-	//skicka bekräftelse tillbaka
-	pubnub.publish({
-        'channel' : 'waitingRoom',
-        'message' : {"mtype": "RES", "sender":userId, "reciever":initiator}
-  	});
-  	enterChat(userId, initiator);
-}
+  function sendMsg (contents) {
+    pubnub.publish({
+      'channel' : chatRoom,
+      'message' : {"contents" : contents, "sender" : userId, "reciever" : chatPartner}
+    });
+  }
 
-function denyRequest(initiator){
-	//slumpad användare blir tillfrågad
-	//skicka bekräftelse tillbaka
-	pubnub.publish({
-        'channel' : 'waitingRoom',
-        'message' : {"mtype": "DEN", "sender":userId, "reciever":initiator}
-  	});
-}
-
-function randomElement() {
-	//Randomizes users array and selects a random element
-	var array = users;
-	for (var id in array) {
-		if (array[id] === userId){
-			array.splice(id,1);
-			break;
-		}
-	}
-	var item = array[Math.floor(Math.random()*array.length)];
-  	return item; //selects a random element
-}
-
-function enterChat(uuidA,uuidB) {
-	//Unsub från public channels
-	//sub to unique (uuidA+UUidB)
-	unsubAll();
-
-}
-function leaveChat() {
-	//unsub från unique/s
-	//call waitingRoom()
-
-}
-
-function getLocation() { //tar fram koordinater
-  	if (navigator.geolocation) {
-      	navigator.geolocation.getCurrentPosition(getCoord);
-  	}
-  	else { 
-      	console.log("Geolocation is not supported by this browser.");
-  	}
-}
-
-function getCoord(position) { //Callback-funktion som hanterar
-	usrLat = position.coords.latitude; //Fullösning att göra dessa publika?
-	usrLng = position.coords.longitude;
-	//geohashChannel = geohash(position.coords.latitude,2) + '' + geohash(position.coords.longitude,2); //Geohashar området till en kanal
-	//console.log(geohashChannel);
-}
+//}
 /*
 
 function subscribe(channel){
