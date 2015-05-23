0b9c24b1337195b511532d02c3c3aeca35fe9f0e
Bophink <Bophink@users.noreply.github.com>
2015-05-23 17:34:26 +0200

    a

--- js/app.js
+++ js/app.js
@@ -2,14 +2,12 @@
 $(function () {
 	//instantierar modellen i appen
 	var model = new Model();
-	console.log(model);
-	//Här instansierar vi resten av views och controllers
+	//Här instantierar vi resten av views och controllers
 	var chatView = new ChatView($("#chat"), model);
-	//var startView = new StartView($("#chat"), model);
 	var shakeCtrl = new ShakeCtrl();
 	var cropCtrl = new CropCtrl(model);
 	var waitingRoomView = new WaitingRoomView($("#chat"), model,shakeCtrl);
 	var profileView = new ProfileView($("#profile"), model, cropCtrl);
-	//var profileCtrl = new ProfileCtrl(profileView, model);
+
 
 });
