90398be565e1ccbb079ed6946597acbf8777c7ec
Calle Sténson <cstenson@kth.se>
2015-05-23 13:44:35 +0200

    kommenterat shakeCtrl

--- js/ctrl/shakeCtrl.js
+++ js/ctrl/shakeCtrl.js
@@ -1,4 +1,7 @@
 var ShakeCtrl = function(){
+	//uses the lib/shake.js 
+	//https://github.com/alexgibson/shake.js/
+	
 	//initiate the shake object.
 	var myShakeEvent = new Shake({
 	  threshold: 15, // optional shake strength threshold
@@ -7,17 +10,13 @@ var ShakeCtrl = function(){
 
 	myShakeEvent.start();
 
-	//function to call when shake occurs
-	this.shakeEventDidOccur = function() {
-	    //put your own code here etc.
-	    window.navigator.vibrate(3000);
-	};
-
+	//activate sake eventlistner with function to fire
 	this.activateShake = function(functionToFire){
 	    //alert("activateShake");
 	    window.addEventListener('shake', functionToFire, false); // ändra till functionToFire
 	};
 
+	//disable shake listner
 	this.disableShake = function(){
 	    //alert("disableShake");
 	    window.removeEventListener('shake', shakeEventDidOccur, false);
